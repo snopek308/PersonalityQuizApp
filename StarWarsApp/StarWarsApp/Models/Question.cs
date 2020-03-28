7fd84504cea5576899144742e1bc4043df83003b
asnopek <asnopek486@gmail.com>
2020-03-28 18:04:43 -0500

    Created the methods to move through the questions and determine which charatcer the user picked

--- StarWarsApp/StarWarsApp/Models/Question.cs
+++ StarWarsApp/StarWarsApp/Models/Question.cs
@@ -6,27 +6,13 @@ namespace StarWarsApp.Models
 {
     class Question
     {
+        public string text { get; set; }
+        public int answer { get; set; }
 
-        public string questionOne { get; set; }
-
-        public string questionTwo { get; set; }
-
-        public string questionThree { get; set; }
-        public string questionFour { get; set; }
-        public string questionFive { get; set; }
-        
-        //left button
-        public string optionOne { get; set; }
-        
-        //right button
-        public string optionTwo { get; set; }
-
-        public string answer { get; set; }
-        
-        //list of questions
-        //string option 1
-        //string option 2
-        //press a button, changing the content
-        //
+        public Question (string t)
+        {
+            this.text = t;
+            this.answer = -1;
+        }
     }
 }
