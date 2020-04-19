bf0d0ed1c47c406a6a853d72eef1ac07e046b062
asnopek <asnopek486@gmail.com>
2020-04-18 23:03:07 -0500

    Created a ListView to ask questions in a list

--- StarWarsApp/StarWarsApp/MainPage.xaml.cs
+++ StarWarsApp/StarWarsApp/MainPage.xaml.cs
@@ -6,29 +6,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using static StarWarsApp.Models.Question;
 
 namespace StarWarsApp
 {
 
-    [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-        QuizDataViewModel qdvm = new QuizDataViewModel();
+
+        public int pointsTotal = 0;
 
         public MainPage()
         {
             InitializeComponent();
 
-            
+            listView.BindingContext = QuizQuestion.All;
         }
 
-        private void Button_Clicked_1(object sender, EventArgs e)
+        public void ScoreQuiz()
         {
+            string CharacterResult = "";
 
+            if(pointsTotal < 0)
+            {
+                CharacterResult = "Ventress";
+            }
+            else if (pointsTotal < 0 && pointsTotal < 50)
+            {
+                CharacterResult = "Hera";
+            }
+            else if (pointsTotal < 50 && pointsTotal < 75)
+            {
+                CharacterResult ="Leia";
+            }
+            else if (pointsTotal < 75 && pointsTotal < 100)
+            {
+                CharacterResult = "Ahsoka";
+            }
+            else
+            {
+                CharacterResult ="Sabine";
+            }
+
+            CharacterName.Text = "Wow, you're such a " + CharacterResult + "!";
+            pointsTotal = 0;
         }
 
-        private void Button_Clicked_2(object sender, EventArgs e)
+        void OnNoButtonClicked(int index)
         {
+            pointsTotal -= 25;
+            QuizQuestion.SetResponse(index, "Y");
+        }
+
+        void OnYesButtonClicked(int index)
+        {
+            pointsTotal += 25;
+            QuizQuestion.SetResponse(index, "Y");
+        }
+
+        void OnItemTapped(System.Object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            if (e == null) return;
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        void OnCellClicked(System.Object sender, System.EventArgs e)
+        {
+            var x = (Button)sender;
+            var a = x.CommandParameter;
+            var q = (QuizQuestion)a;
+
+            if (x.Text == "Left")
+            {
+                OnYesButtonClicked(q.NumberOfQuestion);
+            }
+            else
+            {
+                OnNoButtonClicked(q.NumberOfQuestion);
+            }
+        }
+
+        async void OnScoreQuizClicked(System.Object sender, System.EventArgs e)
+        {
+            // Check that all questions have been answered
+            if (QuizQuestion.AllQuestionsAnswered())
+            {
+                // Score quiz
+                ScoreQuiz();
+            }
+            else
+            {
+                await DisplayAlert("You missed a question!", "Don't be a scruffy looking nerf herder.", "OK");
+            }
 
         }
     }
--- StarWarsApp/StarWarsApp/Models/Question.cs
+++ StarWarsApp/StarWarsApp/Models/Question.cs
@@ -1,18 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace StarWarsApp.Models
 {
     class Question
     {
-        public string text { get; set; }
-        public int answer { get; set; }
 
-        public Question (string t)
+        public class QuizQuestion : INotifyPropertyChanged
         {
-            this.text = t;
-            this.answer = -1;
+            public string Question { get; private set; }
+            public int NumberOfQuestion { get; private set; }
+
+            string UserResponse;
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public string Response
+            {
+                private set
+                {
+                    if (UserResponse != value)
+                    {
+                        UserResponse = value;
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Response"));
+                    }
+                }
+                get
+                {
+                    return UserResponse;
+                }
+            }
+
+            public static IList<QuizQuestion> All { get; set; }
+
+            private QuizQuestion()
+            {
+            }
+
+            public QuizQuestion(string question, int questionNum, string userResponse)
+            {
+                Question = question;
+                NumberOfQuestion = questionNum;
+                UserResponse = userResponse;
+            }
+
+            static QuizQuestion()
+            {
+                List<QuizQuestion> all = new List<QuizQuestion>
+                {
+                    new QuizQuestion("Darkside or Lightside?", 0, null),
+                    new QuizQuestion("Pilot in Space or Pilot a Speeder?", 1, null),
+                    new QuizQuestion("Furry Companion or Human Companion?", 2, null),
+                    new QuizQuestion("Lightsaber or Blaster?", 3, null),
+                    new QuizQuestion("Sassy Droid or Stable Droid?", 4, null),
+                };
+                All = all;
+            }
+
+            public static Boolean AllQuestionsAnswered()
+            {
+                Boolean done = true;
+                foreach (var x in QuizQuestion.All)
+                {
+                    if (x.UserResponse == null)
+                        done = false;
+                }
+
+                return done;
+            }
+
+            public static void SetResponse(int index, string response)
+            {
+                QuizQuestion.All[index].UserResponse = response;
+            }
+
+            public static void ResetQuestions()
+            {
+                foreach (QuizQuestion x in QuizQuestion.All)
+                {
+                    x.UserResponse = null;
+                }
+            }
+
         }
     }
 }
